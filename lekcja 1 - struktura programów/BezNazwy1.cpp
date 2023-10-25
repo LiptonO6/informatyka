8b55d54837f1686421afbfdaa91187a92e0098c9
LiptonO6 <maiky20061@gmail.com>
2023-10-25 14:05:35 +0200

    Pierwa lekcja

---
+++ lekcja 1 - struktura programów/BezNazwy1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+	
+int main(){
+	
+ 	short liczba = 2;
+ 	float zmiennoprzecinkowa = 3.532;
+ 	string tekst = "teskt";
+ 	char znak = 'A';
+ 	bool znakowy = false;
+
+	cout << "polska gurom!!1!11!!" << endl;
+	cout << "Liczba calkowita: " << liczba;
+	cout << endl;
+
+	string jakos;
+	cout << "Podaj jakis wyraz: ";
+	cin >> jakos;
+	cout << "Wprowadziles wartosc: " << jakos;
+
+	return 0;
+}
