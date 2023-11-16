1843ad83fad74448366e647ccc4a73175223d233
LiptonO6 <maiky20061@gmail.com>
2023-11-16 15:02:11 +0100

    zadanie z lekcji

---
+++ lekcja 5 - zadanie/liczby.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	for(int i = 69; i < 71 ; i++){ // i - zmienna lokalna, istnieje tylko w klamrach petli
+		cout << i << endl;
+	}
+	
+cout << endl;
+system("pause");
+}
